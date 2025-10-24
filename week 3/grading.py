d2baa41414992336e42cca702b9c4f19b183d881
yeghia-k <yeghia.kurkjian2@gmail.com>
2025-10-24 14:49:58 +0200

    Create grading.py

---
+++ week 3/grading.py
@@ -0,0 +1,15 @@
+score = int(input("Enter your score (0-100): "))
+if score >= 90:
+grade = "A"
+elif score >= 80:
+grade = "B"
+elif score >= 70:
+grade = "C"
+elif score >= 60:
+grade = "D"
+else:
+grade = "F"
+print(f"Your grade is: {grade}")
+# Bonus: print encouragement for top grades
+if grade == "A":
+print("Excellent work!")
